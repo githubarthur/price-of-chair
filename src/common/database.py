c9628791d17213343c38c926fdc9b06ed2231fd2
pc AA <arthurardani@yahoo.co.id>
2020-09-20 20:33:09 +0700

    change back the database

--- src/common/database.py
+++ src/common/database.py
@@ -12,7 +12,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['fullstack']
+        Database.DATABASE = client['pricing']
 
     @staticmethod
     def insert(collection, data):

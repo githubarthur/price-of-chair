001781ee634386bbf58c2affc645a73ceac9b464
pc AA <arthurardani@yahoo.co.id>
2020-09-20 20:46:32 +0700

    change back the database

--- src/common/database.py
+++ src/common/database.py
@@ -12,7 +12,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['pricing']
+        Database.DATABASE = client.get_database()
 
     @staticmethod
     def insert(collection, data):
